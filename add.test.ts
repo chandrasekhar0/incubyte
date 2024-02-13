c738945417745b13b93dd6a1fcba49ea2ea19d77
chandrasekhar0 <csekhar.saragadam@gmail.com>
2024-02-13 23:47:10 +0530

    test case for pattern "1\n2\n3"

--- add.test.ts
+++ add.test.ts
@@ -6,5 +6,7 @@ describe("test cases related to add method",()=>{
   test('takes input of type String pattern "1,2" and return addition of digits by splitting given string by ,', () => {
     expect(add("1,2")).toBe(3);
   })
-
+  test('takes input of type String pattern "1\n2\n3" and return addition of digits by splitting given string by \n', () => {
+    expect(add("1\n2\n3")).toBe(6);
+  })
 })
