363f8015971f0910a6f7daa5a0b51af520ec43d3
chandrasekhar0 <csekhar.saragadam@gmail.com>
2024-02-13 23:49:49 +0530

    delemiter

--- add.test.ts
+++ add.test.ts
@@ -9,7 +9,7 @@ describe("test cases related to add method",()=>{
   test('takes input of type String pattern "1\n2\n3" and return addition of digits by splitting given string by \n', () => {
     expect(add("1\n2\n3")).toBe(6);
   })
-  test('takes input of type String pattern "1\n2\n3" and return addition of digits by splitting given string by \n', () => {
+  test('takes input of type String pattern "//;\n1;2;9" and return addition of digits by splitting given string by //[delimiter]', () => {
     expect(add("//;\n1;2;9")).toBe(12);
   })
 })
